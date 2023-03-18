67c7cab253eda54ed9644e99f7a49edb261f0df0
Ayodeji Alakija <dejialakija@gmail.com>
2023-03-18 18:14:01 +0100

    my commit

--- src/Routes/Authentication/components/InputField.jsx
+++ src/Routes/Authentication/components/InputField.jsx
@@ -5,7 +5,7 @@ function InputField({ title, type, placeholder }) {
         <div className='my-5'>
             <h1 className='pb-2'>{title}</h1>
             <input type={type} placeholder={placeholder}
-                className={"w-full border-[0.8px] border-gray-300 py-3 p-5 shadow-xl rounded-xl "} />
+                className={"w-full border-[1px] border-purple-400 py-3 p-5 shadow-xl rounded-xl "} />
         </div>
     )
 }
